6b4160746c231360d7f7a6afe7c48d684d2df777
Ahnan Fuadwijaya <ahnanfuadwijaya@gmail.com>
2019-10-07 22:51:09 +0700

    add AboutViewModel Unit Test

---
+++ app/src/test/java/com/fufufu/moviecatalogue/viewmodels/AboutViewModelTest.java
@@ -0,0 +1,16 @@
+package com.fufufu.moviecatalogue.viewmodels;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AboutViewModelTest {
+
+    @Test
+    public void setData() {
+    }
+
+    @Test
+    public void getData() {
+    }
+}
