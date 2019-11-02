c840851be03776eb8c0b842fae9b54b6a24587da
Ahnan Fuadwijaya <ahnanfuadwijaya@gmail.com>
2019-11-02 12:15:25 +0700

    add Favorite Dao and Database class

--- app/src/test/java/com/fufufu/moviecatalogue/viewmodels/AboutViewModelTest.java
+++ app/src/test/java/com/fufufu/moviecatalogue/viewmodels/AboutViewModelTest.java
@@ -1,16 +1,47 @@
 package com.fufufu.moviecatalogue.viewmodels;
 
+import com.fufufu.moviecatalogue.models.About;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ComparisonFailure;
 import org.junit.Test;
+import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.refEq;
 
 public class AboutViewModelTest {
+    private AboutViewModel viewModel;
+    private About testAbout;
 
-    @Test
-    public void setData() {
+    @Before
+    public void setUp() {
+        viewModel = new AboutViewModel();
+        testAbout = new About();
+        testAbout.setName("Ahnan Fuadwijaya");
+        testAbout.setEmail("ahnanfuadwijay@gmail.com");
+        testAbout.setPhoto("profile_photo");
+
     }
 
     @Test
     public void getData() {
+
+        viewModel.setData();
+        About about = viewModel.getData();
+
+        assertNotNull(about);
+
+        assertTrue(assertEquals(testAbout, about));
+    }
+
+    private boolean assertEquals(About a, About b) {
+        if(a.getName().equals(b.getName())){
+            if(a.getEmail().equals(b.getEmail())){
+                return a.getPhoto().equals(b.getPhoto());
+            }
+        }
+        return false;
     }
 }
